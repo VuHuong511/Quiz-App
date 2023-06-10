282508fe1cdbe399dc1f5bfa425bdd8296dd02bc
VuHuong <vuhuong51101@gmail.com>
2023-06-10 14:07:04 +0700

    modify QuizScreen component

--- src/QuizScreen.js
+++ src/QuizScreen.js
@@ -1,31 +1,80 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import { useEffect, useState } from "react";
 
 const QuizScreen = () => {
   const [questions, setQuestions] = useState([]);
-  useEffect(() => {
-    const fetchQuestions = async () => {
-      try {
-        const response = await axios.get(
-          "https://opentdb.com/api.php?amount=5"
-        );
-        setQuestions(response.data.results);
-      } catch (error) {
-        console.error("Error fetching questions:", error);
-      }
-    };
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [selectedAnswer, setSelectedAnswer] = useState("");
+  const [score, setScore] = useState(0);
 
+  useEffect(() => {
     fetchQuestions();
   }, []);
+
+  const fetchQuestions = async () => {
+    try {
+      const response = await fetch("https://opentdb.com/api.php?amount=5");
+      const data = await response.json();
+      setQuestions(data.results);
+    } catch (error) {
+      console.error("Error fetching questions:", error);
+    }
+  };
+
+  const handleAnswerSelection = (answer) => {
+    setSelectedAnswer(answer);
+  };
+
+  const handleNextQuestion = () => {
+    if (selectedAnswer === questions[currentQuestion].correct_answer) {
+      setScore(score + 1);
+    }
+
+    setSelectedAnswer("");
+    setCurrentQuestion(currentQuestion + 1);
+  };
+
+  if (questions.length === 0) {
+    return <div>Loading...</div>;
+  }
+
+  if (currentQuestion >= questions.length) {
+    return (
+      <div>
+        <h2>Quiz Completed!</h2>
+        <p>
+          Your score: {score} / {questions.length}
+        </p>
+      </div>
+    );
+  }
+
+  const currentQuestionData = questions[currentQuestion];
+  const answers = [
+    ...currentQuestionData.incorrect_answers,
+    currentQuestionData.correct_answer,
+  ];
+
   return (
     <div>
-      <h1>QuizScreen</h1>
-      {questions.map((question, index) => (
+      <h2>Question {currentQuestion + 1}</h2>
+      <p dangerouslySetInnerHTML={{ __html: currentQuestionData.question }}></p>
+      {answers.map((answer, index) => (
         <div key={index}>
-          <h2>{question.category}</h2>
-          <p>{question.question}</p>
+          <input
+            type="radio"
+            id={`answer${index}`}
+            name="answer"
+            value={answer}
+            checked={selectedAnswer === answer}
+            onChange={() => handleAnswerSelection(answer)}
+          />
+          <label
+            htmlFor={`answer${index}`}
+            dangerouslySetInnerHTML={{ __html: answer }}
+          ></label>
         </div>
       ))}
+      <button onClick={handleNextQuestion}>Next</button>
     </div>
   );
 };
